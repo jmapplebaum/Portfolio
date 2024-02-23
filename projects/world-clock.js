f029e6deb226804e94ca8513111a8d60f27a06dd
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-22 21:02:17 -0600

    Updated reset cities link on world-clock.js

--- projects/world-clock.js
+++ projects/world-clock.js
@@ -48,7 +48,7 @@ function addCity(event) {
         </div>
         
         <br />
-        <a href="/"> Reset cities
+        <a href="https://jennahcodes.com/projects/world-clock"> Reset cities
       </a>`;
 }
 
